60e7166ee2c4de3d28594848657cf5a939a4d16a
gledfish2 <wenruiyan12@gmail.com>
2022-12-19 16:42:18 +0800

    update

--- data-struction/queue/circular-queue.cpp
+++ data-struction/queue/circular-queue.cpp
@@ -6,13 +6,11 @@ typedef struct circuQueue{
     int front;
     int rear;
     int data[MAX];
-    int capacity;
 }circuQueue, * queuePoint;
 
 void createQueue(queuePoint qp){
     qp ->front = -1;
     qp -> rear = -1;
-    qp ->capacity = 0;
 }
 
 bool isEmpty(queuePoint qp){
@@ -22,13 +20,67 @@ bool isEmpty(queuePoint qp){
         return false;
 }
 bool isFull(queuePoint qp){
-    if(qp -> capacity == MAX)
+    if(qp -> front == 0 && qp -> rear == MAX - 1)
+        return true;
+    else if(qp ->front == qp -> rear + 1)
         return true;
     else
         return false;
 }
 
+bool enQueue(queuePoint qp, int num){
+    if(!isFull(qp)){
+        if(qp -> front == -1)
+            qp -> front++;
+        qp -> rear = (qp -> rear + 1) % MAX;
+        qp -> data[qp -> rear] = num;
+        return true;
+        
+    } else
+        return false;
+
+}
+
+int deQueue(queuePoint qp)
+{
+    int element;
+    if (isEmpty(qp))
+    {
+        cout << "Queue is empty" << endl;
+        return (-1);
+    }
+    else
+    {
+        element = qp -> data[qp -> front];
+        if (qp -> front == 0)
+        {
+            qp -> front = -1;
+            qp -> rear = -1;
+        }
+        else
+        {
+            qp -> front = (qp -> front + 1) % MAX;
+        }
+        return element;
+    }
+}
+void showQueue(queuePoint qp){
+    if(!isEmpty(qp)){
+        for(int i = qp -> front; i != qp -> rear + 1;i = (i + 1) % MAX){
+        cout << qp -> data[i] << endl;
+        }
+    } else{
+        cout << "empty";
+    }
+}
+
 
 int main(void){
+    queuePoint queuePoint = new circuQueue;
+    createQueue(queuePoint);
+    enQueue(queuePoint, 3);
+    enQueue(queuePoint, 5);
+    enQueue(queuePoint, 6);
+    showQueue(queuePoint);
     return 0;
 }
---
+++ data-struction/queue/deque.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+#define Max 10
+#define ERROR 0
+using namespace std;
+
+typedef struct deque
+{
+    int front;
+    int rear;
+    int data[Max];
+} Deque, *dequePoint;
+
+// 初始化队列
+void createdeQue(dequePoint qp)
+{
+    qp->front = -1;
+    qp->rear = -1;
+}
+// 判断队列空
+bool isEmpty(dequePoint qp)
+{
+    if ((qp -> rear == -1) && (qp -> front == -1))
+        return true;
+    else
+        return false;
+}
+// 判断队列满
+bool isFull(dequePoint qp)
+{
+    if ((qp->rear == Max - 1) && (qp->front == 0))
+        return true;
+    else
+        return false;
+}
+
+// 将元素从尾部放入队列
+bool rearEnqueue(dequePoint qp, int num)
+{
+    if (!isFull(qp))
+    {
+        if (!isEmpty(qp))
+        {
+            qp->rear--;
+            qp->data[qp->rear] = num;
+            return true;
+        }
+        else
+        {
+            qp->rear = Max;
+            qp->data[qp->rear] = num;
+            return true;
+        }
+    }
+    else
+        return false;
+}
+// 从队列尾部中取出元素
+int rearDequeue(dequePoint qp)
+{
+    if (!isEmpty(qp))
+    {
+        if (qp->rear == -1)
+        {
+            qp->front--;
+            return qp->data[qp->front + 1];
+        }
+        else
+        {
+            qp->rear--;
+            return qp->data[qp->rear+1];
+        }
+    }
+    else
+        return ERROR;
+}
+
+//从头部插入元素
+bool frontEnqueue(dequePoint qp, int num)
+{
+    if (!isFull(qp))
+    {
+        if (!isEmpty(qp))
+        {
+            qp->front++;
+            qp->data[qp->front] = num;
+            return true;
+        }
+        else
+        {
+            qp->front = 0;
+            qp->data[qp->front] = num;
+            return true;
+        }
+    }
+    else
+        return false;
+}
+// 从队列头部中取出元素
+int frontDequeue(dequePoint qp)
+{
+    if (!isEmpty(qp))
+    {
+        if (qp->front == -1)
+        {
+            qp->rear--;
+            return qp->data[qp->rear + 1];
+        }
+        else
+        {
+            qp->front--;
+            return qp->data[qp->front + 1];
+        }
+    }
+    else
+        return ERROR;
+}
+// 展示队列元素
+void queuePrint(dequePoint qp)
+{
+    if (!isEmpty(qp))
+    {
+        for (int i = qp->rear; i !=  qp->front + 1; i = (i+1)%Max)
+        {
+            cout << qp->data[i] << endl;
+        }
+    }
+}
+
+int main(void)
+{
+    dequePoint q1 = new Deque;
+    createdeQue(q1);
+    cout << isEmpty(q1) << endl;
+    rearEnqueue(q1, 34);
+    rearEnqueue(q1, 43);
+    rearEnqueue(q1, 45);
+    queuePrint(q1);
+    cout << isFull(q1) << endl;
+    rearDequeue(q1);
+    queuePrint(q1);
+    return 0;
+}
