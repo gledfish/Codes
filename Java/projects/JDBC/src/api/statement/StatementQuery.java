3d18896029619bef3aeb6e3c28a1d1b163e2a3a2
gledfish2 <wenruiyan12@gmail.com>
2023-01-30 21:32:09 +0800

    update

---
+++ Java/projects/JDBC/src/api/statement/StatementQuery.java
@@ -0,0 +1,14 @@
+package api.statement;
+
+public class StatementQuery {
+    public static void main(String[] args) {
+
+//        注册驱动
+//        获取连接
+//        创建statement
+//        发送sal语句
+//        结果集解析
+//        关闭资源
+
+    }
+}
